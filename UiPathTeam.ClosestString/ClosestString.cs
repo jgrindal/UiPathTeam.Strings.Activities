54a5e50d06d2e43e24fd2f4aea1b0bc214215d3b
Justin Grindal <justin.grindal@uipath.com>
2018-06-03 22:21:00 -0500

    NFICS:Initial project setup with basic Levenshtein Distance

---
+++ UiPathTeam.ClosestString/ClosestString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace UiPathTeam.ClosestString
+{
+    public class ClosestString : NativeActivity
+    {
+        // Base string to compare against
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> BaseString { get; set; }
+
+        // Array of possible strings to compare against base string
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String[]> Possibles { get; set; }
+
+        // String value with the lowest distance
+        [Category("Output")]
+        public OutArgument<String> SelectedString { get; set; }
+   
+        // Distance from selected string
+        [Category("Output")]
+        public OutArgument<Int32> Distance { get; set; }
+
+        /// <summary>
+        /// Uses Levenshtein Distance to determine the closest string.  Outputs the string and the distance
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Execute(NativeActivityContext context)
+        {
+            Levenshtein comparison = new Levenshtein(BaseString.Get(context));
+
+            foreach(String possible in Possibles.Get(context))
+            {
+                int currentDistance = comparison.Distance(possible);
+                if(currentDistance < Distance.Get(context))
+                {
+                    Distance.Set(context, currentDistance);
+                    SelectedString.Set(context, possible);
+                }
+            }
+        }
+    }
+}
